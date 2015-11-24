3a8cbd44db74a495bdc387254aa2e082d4151b78
thongsav-usgs <thongsav@usgs.gov>
2015-11-24 10:53:03 -0600

    Update to next version so we can tag the new defaults.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'thongsav@usgs.gov'
 license          'gplv3'
 description      'Installs/Configures CIDA Auth DB/Tomcat stack'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 supports         'centos'
 
 depends 'java'
