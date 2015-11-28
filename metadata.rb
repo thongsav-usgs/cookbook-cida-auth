3bb6f12ac8bd293f073578740bc5e221ae51ccc2
thongsav-usgs <thongsav@usgs.gov>
2015-11-27 19:45:53 -0600

    Upgrade to wsi_tomcat 0.1.5 for some bugfixes in data resource

--- metadata.rb
+++ metadata.rb
@@ -4,10 +4,10 @@ maintainer_email 'thongsav@usgs.gov'
 license          'gplv3'
 description      'Installs/Configures CIDA Auth DB/Tomcat stack'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.2'
+version          '0.1.3'
 supports         'centos'
 
 depends 'java'
 depends 'iptables'
-depends 'wsi_tomcat', '>= 0.1.3'
+depends 'wsi_tomcat', '0.1.5'
 depends 'maven'
